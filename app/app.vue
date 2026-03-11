d85a4d99f6def54a6bb199533566d4130a298c56
SnakeGuitar <snakersito08@gmail.com>
2026-03-11 12:23:17 -0600

    Update app.vue

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,13 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <header>
+      <h1>SnakeGuitar Space</h1>
+    </header>
+    
+    <NuxtPage />
+    
+    <footer>
+      <p>@ 2026 SnakeGuitar. All rights reserved.</p>
+    </footer>
   </div>
 </template>
